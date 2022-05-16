877a8b80391e10f29f159314bc5f6c5511784a40
Alan Ruiz <alanj.ruiz@alumnos.udg.mx>
2022-05-16 13:49:31 -0500

    Added swagger documentation

--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const v1 = require('./controllers/v1');
+//Adding Swagger requires
+const swagger= require('swagger-ui-express');
+const swaggerdoc=require('./swagger.json');
+
 
 const server = express();
 const port = 9001;
@@ -10,6 +14,10 @@ server.use(cors(require('./cors.config.json')));
 
 // Routes
 server.use('/v1', v1);
+
+// Adding Swagger documentation
+server.use('/swagger', swagger.serve);
+server.use('/swagger', swagger.setup(swaggerdoc));
 
 
 // Middlewares
