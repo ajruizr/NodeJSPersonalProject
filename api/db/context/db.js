50db81d01e15987fa44acffd8211cb68b07bd79a
Alan Ruiz <alanj.ruiz@alumnos.udg.mx>
2022-05-18 13:37:53 -0500

    added camel case to db connection and removing unnecesary files

--- api/db/context/db.js
+++ api/db/context/db.js
@@ -1,7 +1,24 @@
 // ORM (Object Relational Mapping)
-const pgp = require('pg-promise'); 
+const pgp = require('pg-promise');
+const camelize = require('camelize');
 
-const db = pgp()({
+function camelizeColumnNames(data) {
+    var names = Object.keys(data[0]);
+    var camels = names.map(n=> {
+        return camelize(n);
+    });
+    data.forEach(d=> {
+        names.forEach((n, i)=> {
+            var c = camels[i];
+            if (!(c in d)) {
+                d[c] = d[n];
+                delete d[n];
+            }
+        });
+    });
+}
+
+const db = pgp({receive: camelizeColumnNames})({
     host: 'localhost',
     user: 'postgres',
     password: 'root',
